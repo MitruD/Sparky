c79da6bd5fc879dd2674c1715ca0d7ae0c193aa8
Cronos <dimamitru@gmail.com>
2023-08-17 22:33:12 +0300

    Multiple Assert

--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -25,12 +25,16 @@ namespace Sparky
             //Act
             customer.GreetAndCombineNames("Dima", "Mitru");
             //Assert
-            Assert.AreEqual(customer.GreetMessage, "Hello, Dima Mitru");
-            Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Dima Mitru"));
-            Assert.That(customer.GreetMessage, Does.Contain(","));
-            Assert.That(customer.GreetMessage, Does.StartWith("Hello"));
-            Assert.That(customer.GreetMessage, Does.EndWith("mitru").IgnoreCase);
-            Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(customer.GreetMessage, "Hello, Dima Mitru");
+                Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Dima Mitru"));
+                Assert.That(customer.GreetMessage, Does.Contain(","));
+                Assert.That(customer.GreetMessage, Does.StartWith("Hello"));
+                Assert.That(customer.GreetMessage, Does.EndWith("mitru").IgnoreCase);
+                Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+            });
         }
 
         [Test]
