69d1e75f96972fda7a634cdb2610fd9bfbc96249
Cronos <dimamitru@gmail.com>
2023-08-20 11:35:39 +0300

    MOQ in Action

--- SparkyNUnitTest/BankAccountNUnitTests.cs
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,29 @@ namespace Sparky
     [TestFixture]
     public class BankAccountNUnitTests
     {
-        private BankAccount bankAccount;
+        private BankAccount account;
         [SetUp]
         public void Setup()
         {
-            bankAccount = new(new LogBook());
+
         }
+
+        //[Test]
+        //public void BankDepositLogFakker_Add100_ReturnTrue()
+        //{
+        //    BankAccount bankAccount = new(new LogFakker());
+        //    var result = bankAccount.Deposit(100);
+        //    Assert.IsTrue(result);
+        //    Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        //}
 
         [Test]
         public void BankDeposit_Add100_ReturnTrue()
         {
+            var logMock = new Mock<ILogBook>();
+
+
+            BankAccount bankAccount = new(logMock.Object);
             var result = bankAccount.Deposit(100);
             Assert.IsTrue(result);
             Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
